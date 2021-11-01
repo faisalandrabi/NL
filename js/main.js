0a987ec6508e61ffdfc3a721b30daf5e3878bcbd
faisalandrabi <68887921+faisalandrabi@users.noreply.github.com>
2021-11-01 23:39:38 +0530

    code with lambda api

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const fetchUsers = async() =>
-    await(await fetch('/.netlify/functions/getUsers')).json();
-
+     await(await fetch('/.netlify/functions/getUsers')).json();
+    // await(await fetch('http://localhost:9000/getUsers')).json();
 fetchUsers().then((result) => {
     userTable=document.querySelector('#usertable');
     result.forEach(user => {
